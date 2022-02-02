6d0c41eb1927f13535c12e6ec614554992dad797
bart1259 <bartg1259@gmail.com>
2022-02-01 20:07:09 -0600

    updates frontend

--- frontend/script.js
+++ frontend/script.js
@@ -2,24 +2,79 @@ const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
 const landmarkContainer = document.getElementsByClassName('landmark-grid-container')[0];
+let lastFrame = Date.now();
 
-function drawPoints(frame, landmarks, ctx) {
-    //Draw frame
-    ctx.drawImage(frame, 0, 0, 1280, 720)
-    
-    //Loop over every point
+const WIDTH = 1280;
+const HEIGHT = 720;
+
+function render(frame, landmarks, ctx, flip_x) {
+
+    // Draw Frame
+    if(flip_x) {
+        canvasCtx.save();
+        canvasCtx.scale(-1, 1);
+        canvasCtx.drawImage(frame, 0, 0, -WIDTH, HEIGHT)
+        canvasCtx.restore();
+    } else {
+        canvasCtx.drawImage(frame, 0, 0, WIDTH, HEIGHT);
+    }
+
+    if(!landmarks){
+        return;
+    }
+
+    // Draw points
+    ctx.lineWidth = 5;
     landmarks.forEach(e => {
         ctx.beginPath();
-        ctx.arc(1280 * e.x, 720 * e.y, 5, 0, 2 * Math.PI);
+        ctx.strokeStyle = `rgb(255, ${Math.round(255 * e.visibility)}, ${Math.round(255 * e.visibility)})`;
+        ctx.arc(WIDTH * e.x, HEIGHT * e.y, 5, 0, 2 * Math.PI);
         ctx.stroke();
+    })
+
+    // Draw connections
+    ctx.lineWidth = 2
+    POSE_CONNECTIONS.forEach(line => {
+      // Gradient code
+      let gradient = ctx.createLinearGradient(WIDTH * landmarks[line[0]].x, HEIGHT * landmarks[line[0]].y, WIDTH * landmarks[line[1]].x, HEIGHT * landmarks[line[1]].y);
+      ctx.strokeStyle = gradient;
+      let col1 = `rgb(255, ${Math.round(255 * landmarks[line[0]].visibility)}, ${Math.round(255 * landmarks[line[0]].visibility)})`;
+      let col2 = `rgb(255, ${Math.round(255 * landmarks[line[1]].visibility)}, ${Math.round(255 * landmarks[line[1]].visibility)})`;
+      gradient.addColorStop("0", col1);
+      gradient.addColorStop("1.0", col2);
+
+      // Draw line
+      ctx.beginPath();
+      ctx.moveTo(WIDTH * landmarks[line[0]].x, HEIGHT * landmarks[line[0]].y);
+      ctx.lineTo(WIDTH * landmarks[line[1]].x, HEIGHT * landmarks[line[1]].y);
+      ctx.stroke();
     })
 }
 
+function drawFPS(ctx){
+    // FPS calculations
+    let dt = (Date.now() - lastFrame) / 1000.0;
+    lastFrame = Date.now()
+    let fps = 1 / dt;
+    ctx.font = '25px consolas'
+    ctx.fillStyle = 'rgb(0, 255, 0)'
+    ctx.fillText(`FPS: ${fps.toFixed(1)}`, 10, 26);
+}
+
 function onResults(results) {
-    console.log(results);
-    if(results.poseLandmarks){
-        drawPoints(results.image, results.poseLandmarks, canvasCtx);
+
+    let flip_x = $('.mirrorXCheckbox').is(":checked");
+
+    //Draw frame
+    if(flip_x && results.poseLandmarks) {
+        results.poseLandmarks.forEach(e => {
+            e.x = 1- e.x;
+            return e;
+        })
     }
+
+    render(results.image, results.poseLandmarks, canvasCtx, flip_x);
+    drawFPS(canvasCtx);
 }
 
 const pose = new Pose({locateFile: (file) => {
@@ -39,7 +94,7 @@ const camera = new Camera(videoElement, {
   onFrame: async () => {
     await pose.send({image: videoElement});
   },
-  width: 1280,
-  height: 720
+  width: WIDTH,
+  height: HEIGHT
 });
 camera.start();
